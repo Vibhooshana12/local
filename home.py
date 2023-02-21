21fef08bca7e35c54dff4ae3d5fc6709cfcb37f7
Vibhooshana12 <vibhooshanasenthilkumar@gmail.com>
2023-02-21 17:16:01 +0530

    Fc

---
+++ home.py
@@ -0,0 +1,11 @@
+# Title:Online Medical Shop Management
+# Author: Vibhooshana S
+# Created on:12.02.2023
+# Last modified on:17.02.2023
+# Reviewed by:
+# Reviewed on:
+from Choice import choice_preference
+choice_preference.ch()
+
+
+
